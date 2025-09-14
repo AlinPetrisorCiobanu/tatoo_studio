6517d3385494c154223cabdd3b3207cd7a8f89f6
AlinPC <alinpetrisorciobanu@outlook.es>
2025-09-14 13:20:46 +0200

    Pathc: se da estilo a la vista de login

--- src/pages/Principal/principal.jsx
+++ src/pages/Principal/principal.jsx
@@ -1,36 +1,46 @@
 import Container from 'react-bootstrap/Container';
-import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import "./principal.css"
 
 function Principal() {
 
-  function MyInput() {
-    return (
-      <TextField
-        label="User/Email"
-        variant="outlined"
-        fullWidth
-      />
-    );
-  }
 
   return (
-    <Container fluid="md" className="d-flex justify-content-center align-items-center vh-100">
-      <Row className="justify-content-center align-items-center">
-        <Col xs={11} sm={11} md={11} lg={11} xl={11} xxl={11} className='text-center p-2'>
-          <div className="custom-input">
-            <input type="text" id="userEmail" required />
-            <label htmlFor="userEmail">User/Email</label>
-          </div>
+    <Container fluid="md" className="d-flex justify-content-center align-items-center vh-100 flex-column">
+      <Row className='w-100'>
+        <Col xs={11} sm={11} md={11} lg={11} xl={11} xxl={11} className='text-center p-4 mt-5'>
+          <h1>Tattoo Studio</h1>
         </Col>
-        <hr />
-        <Col xs={11} sm={11} md={11} lg={11} xl={11} xxl={11} className='text-center p-2'>
-          <div className="custom-input">
-            <input type="text" id="password" required />
-            <label htmlFor="password">Password</label>
-          </div>
+      </Row>
+      <Row className="flex-grow-1 d-flex justify-content-center align-items-center">
+        <Col xs={11} sm={11} md={11} lg={11} xl={11} xxl={11}>
+          <Row className="justify-content-center align-items-center">
+            <Col xs={11} sm={11} md={11} lg={11} xl={11} xxl={11} className='text-center p-2'>
+              <div className="custom-input">
+                <input type="text" id="userEmail" required />
+                <label htmlFor="userEmail">User/Email</label>
+              </div>
+            </Col>
+            <hr />
+            <Col xs={11} sm={11} md={11} lg={11} xl={11} xxl={11} className='text-center p-2'>
+              <div className="custom-input">
+                <input type="text" id="password" required />
+                <label htmlFor="password">Password</label>
+              </div>
+            </Col>
+            <hr />
+            <Col xs={11} sm={11} md={11} lg={11} xl={11} xxl={11}>
+              <Row className='justify-content-around align-items-center mt-2'>
+                <Col xs={4} sm={4} md={4} lg={4} xl={4} xxl={4} className='d-flex justify-content-center align-items-center text-center' >
+                <Button className='button_login_reg'>Entrar</Button>
+                </Col>
+                <Col xs={4} sm={4} md={4} lg={4} xl={4} xxl={4} className='d-flex justify-content-center align-items-center text-center' >
+                <Button className='button_login_reg'>Registrar</Button>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
