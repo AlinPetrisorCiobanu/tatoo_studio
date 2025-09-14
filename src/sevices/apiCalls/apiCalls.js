e24bd49ac5c77e2b0343570b1e5cdae9d4798a39
AlinPC <alinpetrisorciobanu@outlook.es>
2025-09-14 14:45:49 +0200

    ADD: se añade funciones para registrar e iniciar sesion de un usuario.

---
+++ src/sevices/apiCalls/apiCalls.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const URL_USER = "https://tatto-studio-backend.vercel.app/"
+
+//users login
+export const login = (data) => {
+  return axios
+    .post(`${URL_USER}login`, data)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+//users register
+export const register = (data) => {
+  return axios
+    .post(`${URL_USER}register`, data)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
